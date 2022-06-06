46bba0649787a2b3f066814738a2033b733585cf
YiranXu <yx1350@nyu.edu>
2022-06-06 17:40:43 +0800

    preorder

---
+++ Leetcode/BinaryTree/144preordertraversal.py
@@ -0,0 +1,30 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        #iterative
+        
+        if not root:
+            return
+        stack,output=[root,],[]
+        while stack:
+            root=stack.pop()
+            if root:
+                output.append(root.val)
+                if root.right:
+                    stack.append(root.right)
+                if root.left:
+                    stack.append(root.left)
+        return output
+#---------------------------
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        def preorder(root):
+            if not root:
+                return
+            result.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        result=[]
+        preorder(root)
+        return result
+        
